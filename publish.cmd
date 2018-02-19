e90276e1f555bfa5bd50ed68e8ed22c61002a543
Camilo Varela <ing.camilovarela@gmail.com>
2018-02-19 14:06:02 -0500

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=springbotty-Web-Deploy.pubxml -p:Password=iaM0mNiht4v3NMMoW5w3g2Ha1zNwXEprjbNuyk4rcvTxMr6hvDfPc2WpeDmJ
+
